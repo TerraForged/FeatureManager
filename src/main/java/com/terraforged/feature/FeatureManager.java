0d5e26c44819c9395af3e5886014d99c6b5983fd
dags- <dags@dags.me>
2020-01-15 12:08:47 +0000

    remove bus

--- src/main/java/com/terraforged/feature/FeatureManager.java
+++ src/main/java/com/terraforged/feature/FeatureManager.java
@@ -11,17 +11,13 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.BusBuilder;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class FeatureManager implements FeatureDecorator {
-
-    public static final IEventBus BUS = BusBuilder.builder().setTrackPhases(false).build();
-
+    
     private final Map<Biome, BiomeFeatures> biomes;
 
     public FeatureManager(Map<Biome, BiomeFeatures> biomes) {
